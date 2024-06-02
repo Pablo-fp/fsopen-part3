443c216e9d0b8166a634863dad1329f9910f42da
Pablo-fp <pablofp1988@hotmail.com>
2024-06-02 09:18:39 +0200

    Phonebook backend step 8

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -2,9 +2,21 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
+// custom token for morgan
+morgan.token("postData", function (req) {
+  if (req.method === "POST") {
+    return JSON.stringify(req.body);
+  }
+});
+
 //Middlewares
 app.use(express.json());
-app.use(morgan("tiny"));
+//app.use(morgan("tiny"));
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :postData"
+  )
+);
 
 let persons = [
   {
