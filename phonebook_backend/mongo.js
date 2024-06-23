0d9e347c883d8bcfee2282ef07be8276b3ecfe99
Pablo-fp <pablofp1988@hotmail.com>
2024-06-23 20:24:24 +0200

    3.12: Command-line database

--- phonebook_backend/mongo.js
+++ phonebook_backend/mongo.js
@@ -1,31 +1,49 @@
 const mongoose = require("mongoose");
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password> <name> <number>"
+  );
+  console.log("To print phone book enter: node mongo.js <password>");
   process.exit(1);
 }
 
-const password = process.argv[2];
+const [password, name, number] = process.argv.slice(2);
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`;
+const url = `mongodb+srv://fullstack:${password}@cluster0.opeqixu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose.set("strictQuery", false);
 
-mongoose.connect(url);
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 });
 
-const Note = mongoose.model("Note", noteSchema);
+const Person = mongoose.model("Person", personSchema);
 
-const note = new Note({
-  content: "HTML is easy",
-  important: true,
-});
-
-note.save().then((result) => {
-  console.log("note saved!");
-  mongoose.connection.close();
-});
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected");
+    if (name && number) {
+      const person = new Person({
+        name,
+        number,
+      });
+      return person.save().then((person) => {
+        console.log(
+          `added ${person.name} number ${person.number} to phonebook`
+        );
+        mongoose.connection.close();
+      });
+    }
+    // write phonebook
+    console.log("phonebook:");
+    Person.find({}).then((persons) => {
+      persons.map((person) => console.log(person.name, person.number));
+      mongoose.connection.close();
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
